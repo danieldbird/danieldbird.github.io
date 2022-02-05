e9865b629f30c05cdd666b0ddbf0a27a51646c8e
Daniel Bird <danieldbird@gmail.com>
2022-02-05 21:41:38 +1300

    added margins to index headings

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const technologies = ["HTML", "CSS", "Javascript"];
+
+const technologyElement = document.querySelector(".technology-highlight");
+
+technologyElement.innerHTML = technologies[0];
+
+let counter = 1;
+setInterval(() => {
+  // technologyElement.style.opacity = 1;
+  technologyElement.innerHTML = technologies[counter];
+  // technologyElement.style.opacity = 0;
+  if (counter === technologies.length - 1) {
+    counter = 0;
+  } else {
+    counter++;
+  }
+}, 2000);
+
+console.log(technologyElement);
