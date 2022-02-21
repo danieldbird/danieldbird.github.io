339627227b661fd7183f1a0363621d1837831308
Daniel Bird <danieldbird@gmail.com>
2022-02-21 13:05:28 +1300

    add particle system animation

--- js/particles.js
+++ js/particles.js
@@ -1,39 +1,116 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = document.body.scrollHeight;
+let width = document.body.clientWidth;
+let height = document.body.scrollHeight;
+canvas.width = width;
+canvas.height = height;
 
-const particles = 1000;
+// fix margin appearing at the bottom of page because everything hadn't loaded yet
+window.addEventListener("load", () => {
+  width = document.body.clientWidth;
+  height = document.body.scrollHeight;
+  canvas.width = width;
+  canvas.height = height;
+});
 
+// add a resize listener to update canvas size
+window.addEventListener("resize", () => {
+  setTimeout(() => {
+    width = document.body.clientWidth;
+    height = document.body.scrollHeight;
+    canvas.width = width;
+    canvas.height = height;
+  }, 200);
+});
+
+// set defaults
+const amount = 500;
+const minSize = 1;
+const maxSize = 3;
+const minVel = 0.1;
+const maxVel = 0.5;
+const minAlpha = 0.5;
+const maxAlpha = 1;
+const color = "#a363bb";
+const particles = [];
+
+// utilities
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function getRandomFloat(min, max) {
-  return Number(Math.random() * (max - min) + min).toFixed(1);
+  return Number(Math.random() * (max - min) + min);
 }
 
-function createParticle() {
-  let size = getRandomInt(2, 3);
-  ctx.fillStyle = "#a363bb";
-  ctx.globalAlpha = getRandomFloat(0.25, 0.75);
-  ctx.fillRect(
-    Math.floor(Math.random() * window.innerWidth),
-    Math.floor(Math.random() * document.body.scrollHeight),
-    size,
-    size
-  );
-}
-
-for (let i = 0; i < particles; i++) {
-  createParticle();
-}
-
-window.addEventListener("resize", () => {
-  canvas.width = window.innerWidth;
-  canvas.height = document.body.scrollHeight;
-  for (let i = 0; i < particles; i++) {
-    createParticle();
+const getRandomVelocity = (posOrNeg) => {
+  if (posOrNeg) {
+    if (posOrNeg === "+") {
+      return getRandomFloat(minVel, maxVel);
+    } else if (posOrNeg === "-") {
+      return Number(-getRandomFloat(minVel, maxVel));
+    }
+  } else {
+    if (Math.random() < 0.5) {
+      return Number(-getRandomFloat(minVel, maxVel));
+    }
+    return getRandomFloat(minVel, maxVel);
   }
-});
+};
+
+// create particles
+function createParticles() {
+  for (let i = 0; i < amount; i++) {
+    particles.push({
+      posX: getRandomFloat(0, width),
+      posY: getRandomFloat(0, height),
+      velX: getRandomVelocity(),
+      velY: getRandomVelocity(),
+      size: getRandomInt(minSize, maxSize),
+      alpha: Number(getRandomFloat(minAlpha, maxAlpha).toFixed(2)),
+    });
+  }
+}
+
+createParticles();
+
+// draw particles
+function animateParticles() {
+  ctx.clearRect(0, 0, width, height);
+  for (let i = 0; i < particles.length; i++) {
+    ctx.fillStyle = color;
+    ctx.globalAlpha = particles[i].alpha;
+    ctx.fillRect(
+      (particles[i].posX += particles[i].velX),
+      (particles[i].posY += particles[i].velY),
+      particles[i].size,
+      particles[i].size
+    );
+    if (particles[i].posX <= -maxSize) {
+      particles[i].posY = getRandomFloat(0, height);
+      particles[i].velX = getRandomVelocity("+");
+      particles[i].velY = getRandomVelocity("+");
+    }
+    if (particles[i].posX >= width + maxSize) {
+      particles[i].posX = width;
+      particles[i].posY = getRandomFloat(0, height);
+      particles[i].velX = getRandomVelocity("-");
+      particles[i].velY = getRandomVelocity("-");
+    }
+    if (particles[i].posY <= -maxSize) {
+      particles[i].posX = getRandomFloat(0, width);
+      particles[i].posY = height;
+      particles[i].velX = getRandomVelocity("+");
+      particles[i].velY = getRandomVelocity("+");
+    }
+
+    if (particles[i].posY >= height + maxSize) {
+      particles[i].posX = getRandomFloat(0, width);
+      particles[i].velX = getRandomVelocity("-");
+      particles[i].velY = getRandomVelocity("-");
+    }
+  }
+  requestAnimationFrame(animateParticles);
+}
+requestAnimationFrame(animateParticles);
